c78e61ff27f6a8fbfd1e475be4476aa11579c761
Richard Hladík <rihl@uralyx.cz>
2016-10-29 16:55:14 +0200

    fastexp: správně zacházet s nezmodulenými a zápornými základy

--- src/numth/misc.cpp
+++ src/numth/misc.cpp
@@ -3,6 +3,7 @@
 /* Fast exponentiation modulo mod
  * Time complexity: O(log p), space complexity: O(1) */
 ll fastexp(ll a, ll p, ll mod) {
+	a = ((a % mod) + mod) % mod;
 	ll res = 1 % mod;
 	for (; p; a = (a * a) % mod, p /= 2)
 		if (p % 2)
