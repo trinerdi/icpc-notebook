6fbe306a52ff7bbf78e2e873c267174779df9e44
Václav <vaclav.volhejn@gmail.com>
2016-11-13 13:48:08 +0100

    Kratší LCA, dokumentace

--- src/tree/lca.cpp
+++ src/tree/lca.cpp
@@ -1,8 +1,10 @@
 #include "../base.hpp"
 
+// Assumes a tree in adjacency list form is saved in g Call initLCA first.
+// O(n log n) initialization, O(log n) query. Good constants on both.
+
 const int N = 2e5 + 10;
 const int LOG = 20;
-
 int d[N];
 int pred[N][LOG];
 vector<int> g[N];
@@ -19,29 +21,21 @@ void dfs(int a) {
 void initLCA(int root, int n = N) {
     pred[root][0] = -1, d[root] = 0;
     dfs(root);
-    rep(l, 1, LOG) {
-        rep(i, 0, n) {
-            if (pred[i][l - 1] == -1) pred[i][l] = -1;
-            else pred[i][l] = pred[pred[i][l - 1]][l - 1];
-        }
+    rep(l, 1, LOG) rep(i, 0, n) {
+        if (pred[i][l - 1] == -1) pred[i][l] = -1;
+        else pred[i][l] = pred[pred[i][l - 1]][l - 1];
     }
 }
 
 int lca(int a, int b) {
     if (d[a] < d[b]) swap(a, b);
-    int l = LOG - 1;
-    while (l >= 0) {
-        if (pred[a][l] == -1 || d[pred[a][l]] < d[b]) l--;
-        else a = pred[a][l];
+    per(l, 0, LOG) {
+        if (pred[a][l] != -1 && d[pred[a][l]] >= d[b]) a = pred[a][l];
     }
     if (a == b) return a;
-    l = LOG - 1;
-    while (l >= 0) {
-        if (pred[a][l] != pred[b][l]) {
-            a = pred[a][l];
-            b = pred[b][l];
-        }
-        l--;
+    per(l, 0, LOG) if (pred[a][l] != pred[b][l]) {
+        a = pred[a][l];
+        b = pred[b][l];
     }
     return pred[a][0];
 }
