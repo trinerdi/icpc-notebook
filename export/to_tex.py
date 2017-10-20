b55f12ad20b480e8ae648e4ab1f0c18079ee9965
Richard Hladík <rihl@uralyx.cz>
2017-10-20 22:38:21 +0200

    Exportítko: pythoní TeXovač: zrychlit, okomentovat, vyčistit výstup
    Opravena chyba v regexech, kvůli které se nějaká replace musela volat
    vícekrát (správné nahrazení dvou zvýrazňovaných slov za sebou), výrazně
    snížen počet volání regexového nahradítka.

--- export/to_tex.py
+++ export/to_tex.py
@@ -1,25 +1,19 @@
 import re
+import sys
 
 
-# Reads the whole stdin
-def readin():
-    l = []
-    while True:
-        try:
-            l.append(input())
-        except EOFError:
-            return "\n".join(l)
-
-
-text = readin()
+text = sys.stdin.read()
 print("\\verb") # start verbatim
 
+# Handle whitespace, blank lines and special characters
 text = re.sub("^ +$", "", text)
 for i in [("\\", "\\b "), ("{", "\\{"), ("}", "\\}"), ("\n\\}\n\n", "\n\\}\\greatsk\n"), ("\n\n", "\n\\sk\n")]:
     text = text.replace(i[0], i[1])
 
+
 def highlight(text):
     def sp(begin, end, name, t):
+        """Highlight the first occurence of [begin]text[end] in text."""
         if begin not in t:
             return None
 
@@ -28,11 +22,15 @@ def highlight(text):
         if len(post) == 1:
             post.append("")
 
+        pre, text, post = pre, post[0], post[1]
+
         # The recursion level will be at most 5
-        return (highlight(pre) + "\\%s{%s%s%s}" % (name, begin, post[0], end), post[1])
+        return (highlight(pre) + "\\%s{%s%s%s}" % (name, begin, text, end), post)
 
     l = []
     while True:
+        # The order is important: /* "string" */ will be highlighted as a comment
+        # TODO: preprocessor directives should be ^#.*, but we don't support regexes here
         for b, e, n in [("//", "\n", "com"), ("/*", "*/", "com"), ("#", "\n", "dir"), ("\"", "\"", "str"), ("'", "'", "str")]:
             res = sp(b, e, n, text)
             if res is not None:
@@ -42,24 +40,23 @@ def highlight(text):
         else:
             break
 
-    def repl(word, name, text):
-        return re.sub(r"(^|\W)(%s)($|\W)" % word, r"\1\\%s{%s}\3" % (name, word), text)
+    def repl(words, name, text):
+        return re.sub(r"(^|\W)(%s)((?= )|$|\W)" % words, r"\1\\%s{\2}\3" % (name), text)
 
 
+    # Types, templated types, keywords, constants
     d = {
-        "typ": "bool char signed unsigned short long int ll ld float double void auto string ull".split(),
-        "tem": "array vector deque forward_list list set map multiset multimap unordered_set unordered_map unordered_multiset unordered_multimap stack queue priority_queue pair".split(),
-        "key": "break case class const continue delete do else for friend goto if inline namespace new private public return static struct switch this typedef using while rep per FOR".split(),
-        "con": "true false PI pi INF inf EPS eps NULL MOD MAXN MAXM".split()
+        "typ": "bool char signed unsigned short long int ll ld float double void auto string ull",
+        "tem": "array vector deque forward_list list set map multiset multimap unordered_set unordered_map unordered_multiset unordered_multimap stack queue priority_queue pair",
+        "key": "break case class const continue delete do else for friend goto if inline namespace new private public return static struct switch this typedef using while rep per FOR",
+        "con": "true false PI pi INF inf EPS eps NULL MOD MAXN MAXM"
     }
 
     for name in d.keys():
-        for kw in d[name]:
-            text = repl(kw, name, text)
-            text = repl(kw, name, text)
+        text = repl(d[name].replace(" ", "|"), name, text)
 
 
-    text = re.sub(r"(^|\W)(-?\d+(U?L?L|(\.\d+)?(e-?\d+)?))($|\W)", r"\1\\con{\2}\6", text)
+    text = re.sub(r"(^|\W)(-?\d+(U?L?L|(\.\d+)?(e-?\d+)?))((?= )|$|\W)", r"\1\\con{\2}\6", text)
     l.append(text)
     return "".join(l)
 
