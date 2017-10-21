3c8807d730c4a477eb7f087a9e75bf3e1b6b368d
Richard Hladík <rihl@uralyx.cz>
2017-10-21 09:00:02 +0200

    Zjednodušit gcd a extgcd

--- src/numth/modeq.cpp
+++ src/numth/modeq.cpp
@@ -4,26 +4,21 @@
  * v such that a * u + b * v = gcd(a, b)
  * Time complexity: O(log a + log b), space complexity: O(1) */
 pair<ll, ll> extgcd(ll a, ll b) {
-	ll Aa = 1, Ab = 0; // a' = Aa * a + Ab * b
-	ll Ba = 0, Bb = 1; // b' = Ba * a + Bb * b
+	complex<ll> A = {1, 0}, B = {0, 1};
+	// a' = R(A) * a + I(A) * b, b' = R(B) * b + I(B) * a
 
-	while (b) {
-		ll c = a / b;
-		Aa -= c * Ba;
-		Ab -= c * Bb;
-		a = a % b;
-		swap(Aa, Ba), swap(Ab, Bb), swap(a, b);
-	}
+	while (b)
+		A -= (a / b) * B, a %= b, swap(a, b), swap(A, B);
 
-	return {Aa, Ab};
+	return {real(A), imag(A)};
 }
 
 /* "Regular" Euclid's algorithm that returns gcd(a, b)
  * Time complexity: O(log a + log b), space complexity: O(1) */
 ll gcd(ll a, ll b) {
-	ll u, v;
-	tie(u, v) = extgcd(a, b);
-	return u * a + v * b;
+	while (b)
+		a %= b, swap(a, b);
+	return a;
 }
 
 /* Solves the modular equality ak \equiv b (mod m), returns 0 <= k < m or -1
