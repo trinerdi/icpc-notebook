81b9ab69d21a7ab85bda05996c378e6eafa2c8a9
Richard Hladík <rihl@uralyx.cz>
2017-10-27 11:45:17 +0200

    Checksum script: Fix the handling of backslashes

--- src/check.sh
+++ src/check.sh
@@ -1,5 +1,5 @@
 IFS=
-while read a; do
+while read -r a; do
 	# Doesn't really work in general (multiline comments are broken, ...), but works well enough
 	printf "%s  %s\n" "$(echo "$a" | sed -re 's/\s*([^a-zA-Z_])\s*/\1/g;s/.*\*\/|\/[/*].*|^\s*|\s*$//g' | md5sum | head -c3)" "$a"
 done
