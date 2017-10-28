a0010400a6703fdc28231b37732bd624ac39343c
Richard Hladík <rihl@uralyx.cz>
2017-10-28 18:15:08 +0200

    Merge the line-hashing functionality of check.sh into sc.sh

--- src/sc.sh
+++ src/sc.sh
@@ -16,3 +16,10 @@ e(){
 	b "$@"
 	for i in "$(name)".in*; do echo $i:; c "$@" < $i; done
 }
+check(){
+	{ IFS=
+	while read -r a; do
+		# Doesn't really work in general (multiline comments are broken, ...), but works well enough
+		printf "%s  %s\n" "$(echo "$a" | sed -re 's/\s+|\/\/.*//g' | md5sum | head -c3)" "$a"
+	done } < "$(name)".cpp
+}
