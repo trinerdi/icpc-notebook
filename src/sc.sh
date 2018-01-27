034623759c610ff4df5586a3c45526f74d3e7520
Richard Hladík <rihl@uralyx.cz>
2018-01-27 17:38:47 +0100

    Sc.sh modifications (#25)
    * The sc.sh script: add n() which inits a directory for a new task
    
    * The sc.sh script: remove run() (e() should be used instead), inline c()
    
    * Shorten the code a little in sc.sh
    
    * Remove b from sc.sh whcih allows for further reduction
    
    I think it almost never occurs that it is needed to compile a solution
    without running it on sample tests.

--- src/sc.sh
+++ src/sc.sh
@@ -1,18 +1,12 @@
-name(){
-	basename `realpath .`
-}
+alias t="basename \$(realpath .)" # Assumption: prints an alphanumeric string
 b(){
-	flags=-O2\ -fsanitize=address
-	[ a"$1" = a-d ] && flags=-g
-	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $flags "$(name)".cpp -o "$(name)"
+	fl=-O2\ -fsanitize=address
+	[ a"$1" = a-d ] && fl=-g && v=valgrind
+	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $fl `t`.cpp -o `t`
+	for i in `t`.in*; do echo $i:; $v ./`t` < $i; done
 }
-c(){
-	`[ a"$1" = a-d ] && echo valgrind` ./"$(name)"
-}
-run(){
-	b "$@" && c "$@"
-}
-e(){
-	b "$@"
-	for i in "$(name)".in*; do echo $i:; c "$@" < $i; done
+n(){
+	mkdir "$1"
+	cd "$1"
+	cp -rn ../template.cpp "$1".cpp
 }
