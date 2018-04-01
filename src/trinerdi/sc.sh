05e5a7631ed075633fe9bd2e45e622359ba71183
Richard Hladík <rihl@uralyx.cz>
2018-04-01 21:39:23 +0200

    Update sc.sh (#52)
    * sc.sh: Fix flags being set incorrectly under some circumstances
    
    Since variables set in a function are visible globally, setting
    v=valgrind once resulted in v retaining the value for later invokations.
    (Therefore, calling `b` after that would result in compiling the binary
    with -fsanitize=address and then running it with Valgrind, which
    complained about -fsanitize=address.)
    
    * sc.sh: Rename the functions; create all problem folders at once
    
    Assuming template.cpp is correctly transcribed the first time, it saves
    time to have all the folders created at once. Now we also `touch` a
    sample input file.
    
    * sc.sh: address comments
    
    * sc.sh: drop redundant cp flag

--- src/trinerdi/sc.sh
+++ src/trinerdi/sc.sh
@@ -1,12 +1,13 @@
-alias t="basename \$(realpath .)" # Assumption: prints an alphanumeric string
-b(){
-	fl=-O2\ -fsanitize=address
-	[ a"$1" = a-d ] && fl=-g && v=valgrind
-	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $fl `t`.cpp -o `t`
-	for i in `t`.in*; do echo $i:; $v ./`t` < $i; done
+e(){ # run on [problem].in*
+	t=$(basename `pwd`)
+	if [ "$1" = -d ]; then fl=-g; v=valgrind
+	else fl=-O2\ -fsanitize=address; v=; fi
+	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $fl $t.cpp -o $t
+	for i in $t.in*; do echo $i:; $v ./$t < $i; done
 }
-n(){
-	mkdir "$1"
-	cd "$1"
-	cp -rn ../template.cpp "$1".cpp
+create(){
+	for i in {a..z}; do  # Change z as needed
+		mkdir "$i" && cd "$i" || continue
+		cp -n ../template.cpp "$i".cpp; touch "$i".in1; cd ..
+	done
 }
