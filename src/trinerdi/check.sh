f6d2f50c1b0b2e7b1c67c66aae29d0b48d1a130f
Václav <vaclav.volhejn@gmail.com>
2018-04-08 13:24:41 +0200

    Add checker to PDF

--- src/trinerdi/check.sh
+++ src/trinerdi/check.sh
@@ -1,5 +1,5 @@
 IFS=
 while read -r a; do
 	# Doesn't really work in general (multiline comments are broken, ...), but works well enough
-	printf "%s  %s\n" "$(echo "$a" | sed -re 's/\s+|\/\/.*//g' | md5sum | head -c3)" "$a"
+	printf "%s  %s\n" "$(echo "$a" | sed -re 's/\s+|\/\/.*//g' | md5sum | head -c2)" "$a"
 done
