c305484f2c4b338d0c59ce2f1e0fc47c5ee9d252
Richard Hladík <rihl@uralyx.cz>
2020-02-25 13:18:19 +0100

    Tooling updates (#79)
    Update the non-algorithmic part of the notebook a bit:
    
    *    The build script does not recompile an unchanged file (+ other small changes)
    *    Added a vimrc
    *    Replaced base.hpp with a proper template.cpp

--- src/trinerdi/sc.sh
+++ src/trinerdi/sc.sh
@@ -1,9 +1,15 @@
-e(){ # run on [problem].in*
+# Usage: e [-d] test1.in … testN.in
+# Compiles the solution (if it has changed since the last time) and runs it on test1.in … testN.in.
+# If no tests are given, runs the solution on all files whose name contains 'in'.
+# If -d is given, compiles the solution with debug flags and runs it with valgrind.
+e(){
 	t=$(basename `pwd`)
-	if [ "$1" = -d ]; then fl=-g; v=valgrind
-	else fl=-O2\ -fsanitize=address; v=; fi
-	g++ -std=c++11 -lm -Wall -Wno-sign-compare -Wshadow $fl $t.cpp -o $t
-	for i in $t.in*; do echo $i:; $v ./$t < $i; done
+	if [ "$1" = -d ]; then fl=-g; v=valgrind; d=$t-debug; shift
+	else fl=-O2\ -fsanitize=address; v=; d=$t; fi
+	if [ $# = 0 ]; then args=*in*
+	else args="$@"; fi
+	[ $t.cpp -nt $d ] && ( g++ -std=c++14 -lm -Wall -Wextra -Wno-sign-compare -Wshadow -DLOCAL $fl $t.cpp -o $d || return )
+	for i in $args; do echo $i:; $v ./$d < $i; done
 }
 create(){
 	for i in {a..z}; do  # Change z as needed
---
+++ src/trinerdi/template.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+#define rep(i, a, n) for (int i = (a); i < (n); i++)
+#define per(i, a, n) for (int i = (n) - 1; i >= (a); i--)
+
+int main(void) {
+	ios_base::sync_with_stdio(0);
+}
